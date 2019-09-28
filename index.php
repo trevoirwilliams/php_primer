0c9136b6e8741a72134ab89ab0ca762879dbb3ac
Trevoir Williams <trevoir.williams@gmail.com>
2019-09-28 17:39:11 +0000

    Added Link to Heroku

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <button type="button" class="btn btn-dark">CLICK ME!</button>
     <button type="button" class="btn btn-primary">CLICK ME!</button>
     <button type="button" class="btn btn-success">CLICK ME!</button>
-
+    <a href="https://www.heroku.com" target="_blank" class="btn btn-danger">Heroku.com</a>
     <?php
         require 'includes/footer.php' 
     ?>
